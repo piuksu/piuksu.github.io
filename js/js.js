665cf4e8888e9dcf0e8d612e0727862fdda2a4fc
pg255 <raikala255@gmail.com>
2025-01-10 11:56:28 +0200

    Website was Outdated

--- js/js.js
+++
@@ -1,6 +0,0 @@
-function oa(link) {
-    window.location.assign(link);
-}
-function ac(nimi, vaartus) {
-    document.cookie = nimi + "=" + vaartus + ";expires=Sat, 13 Aug 2100 18:15:49 GMT;path=/";
-}
